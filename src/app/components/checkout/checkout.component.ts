4d3470e2d150b4d5c14ba73cad7cd8213f1d7efb
Oana <oana.apvel@gmail.com>
2024-01-15 21:43:18 +0200

    Create layout for checkout form

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent {
+  checkoutFormGroup!: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      }),
+    });
+  }
+
+  onSubmit() {
+    console.log("Test");
+  }
+
+  copyShippingAddressToBillingAddress(event: any) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+            .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+    
+  }
+
+}
