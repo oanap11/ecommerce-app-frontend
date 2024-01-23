9fe0f3df594a3f73e4638403312998f32d3a27df
Oana <oana.apvel@gmail.com>
2024-01-23 15:03:58 +0200

    Create classes for Order, Customer, Purchase, Address, CartItem, OrderItem

---
+++ src/app/common/purchase/purchase.ts
@@ -0,0 +1,12 @@
+import { Address } from "../address/address";
+import { Customer } from "../customer/customer";
+import { OrderItem } from "../order-item/order-item";
+import { Order } from "../order/order";
+
+export class Purchase {
+    customer!: Customer;
+    shippingAddress!: Address;
+    billingAddress!: Address;
+    order!: Order;
+    orderItems!: OrderItem[];
+}
